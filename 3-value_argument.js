baa74451e19afd2fd6c215d4b71323304ceeb7a6
Blessing James <blessingjam9@gmail.comgit config --listgit config --global user.email blessingjam9@gmail.com>
2025-07-13 16:30:33 +0100

    3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,7 +2,7 @@ const res = process.argv.slice(2);
 const per = res[0];
 
 if (per === undefined) {
-    console.log("no argument");
+    console.log("No argument");
 } else {
     console.log(per);
 }
